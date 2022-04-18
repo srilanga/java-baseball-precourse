4fee877fe1f1c3498d17799b33c834e2bc5fe74c
srilanga <2983rgt@gmail.com>
2022-04-18 23:12:29 +0900

    fix: ApplicationTest 2가지 수행 및 통과하도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,10 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-import static com.sun.corba.se.impl.activation.ServerMain.printResult;
 
 public class Application {
     public static void main(String[] args) {
@@ -15,20 +13,13 @@ public class Application {
         boolean again = true;
 
         while(again) {
-            try{
-                doGame();
-            } catch(IllegalArgumentException e){
-                System.out.println("1 또는 2를 입력하세요.\n게임을 새로 시작하려면 1, 종료하려면 2.");
-            } catch(Exception e){
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
+            doGame();
 
             // 게임 재시작 여부 확인
             again = playAgain();
         }
 
-        System.out.println("게임이 종료되었습니다.");
+        System.out.println("게임 종료");
     }
 
     private static boolean playAgain() {
@@ -47,17 +38,17 @@ public class Application {
         }
 
         // 그 외의 경우(사용자 입력값이 1,2가 아닌 경우), false 반환 대신, IAException 발생
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("1 또는 2를 입력하세요.\n게임을 새로 시작하려면 1, 종료하려면 2.");
 
     }
 
     public static void doGame(){
         List<Integer> arrComputer = new ArrayList<>(3);    //컴퓨터 값(랜덤값) 저장 배열
-        List<Integer> arrPlayer = new ArrayList<>(3);  //사용자 값(입력값) 저장 배열
+        List<Integer> arrPlayer = null;  //사용자 값(입력값) 저장 배열
 
         // 3자리 랜덤 숫자 생성(camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange())
         for (int i=0; i<3; i++){
-            arrComputer.add(pickNumberInRange(0,9));
+            arrComputer.add(Randoms.pickNumberInRange(0,9));
         }
 
         int strike; //스트라이크 갯수
@@ -75,11 +66,7 @@ public class Application {
             userNum = Console.readLine();
 
             // 사용자 입력값이 3자리가 아닌 경우, illegal 처리
-            try{
-                checkUserNum(userNum);
-            } catch(IllegalArgumentException e){
-                System.out.println("3자리 숫자를 입력해주세요.");
-            }
+            checkUserNum(userNum);
 
             // 사용자 입력값의 자릿수를 배열에 저장
             arrPlayer = saveUserNum(userNum, arrPlayer);
@@ -120,8 +107,9 @@ public class Application {
     }
 
     public static List<Integer> saveUserNum(String userNum, List<Integer> arrPlayer){
-        for(int i=0; i<arrPlayer.size(); i++){
-            arrPlayer.add(userNum.charAt(i)-48);
+        arrPlayer = new ArrayList<>();
+        for(String number : userNum.split("")){
+            arrPlayer.add(Integer.parseInt(number));
         }
         return arrPlayer;
     }
